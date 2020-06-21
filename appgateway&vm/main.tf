364c3a3ac53a46cbf2f11cab209e4d838c859ccd
sreerajdss <sreerajds@gmail.com>
2020-06-21 14:00:23 +0200

    Update main.tf
    added features {}

--- appgateway&vm/main.tf
+++ appgateway&vm/main.tf
@@ -1,8 +1,9 @@
-﻿provider "azurerm" {
+provider "azurerm" {
     subscription_id = "${var.subscription_id}"
     client_id       = "${var.client_id}"
     client_secret   = "${var.client_secret}"
     tenant_id       = "${var.tenant_id}"
+    features {}    
 } 
 
 resource "azurerm_resource_group" "rg" {
